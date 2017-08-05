4bc80f5c333ccc09ad1c7680965ef951cc67d01a
Sanjeev Shrestha <sanjeev.shrestha@walmart.com>
2017-08-05 14:49:38 -0400

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { MetricRegistry } from './lib/metric-registry';
+
+const metricRegistry: MetricRegistry = new MetricRegistry("Sanjeev");
+
+metricRegistry.sayName();
---
+++ src/lib/metric-registry.ts
@@ -0,0 +1,9 @@
+export class MetricRegistry {
+    constructor(private name: string) {
+
+    }
+
+    public sayName() {
+        console.log(this.name)
+    }
+}
